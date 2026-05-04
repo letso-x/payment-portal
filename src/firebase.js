4edadfe33a3561b17be7c9c204e3236e4dd74efc
Tshireletso Seqeta <seqeta@gmail.com>
2026-05-04 13:51:38 +0200

    renamed VITE_.env to just .env and fixed type in firebase.js: changed apikey using camel casing to apiKey

--- src/firebase.js
+++ src/firebase.js
@@ -9,7 +9,7 @@ import { getFirestore } from "firebase/firestore";
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-apikey: import.meta.env.VITE_API_KEY, 
+apiKey: import.meta.env.VITE_API_KEY, 
 authDomain: import.meta.env.VITE_AUTH_DOMAIN, 
 projectId: import.meta.env.VITE_PROJECT_ID, 
 storageBucket: import.meta.env.VITE_STORAGE_BUCKET, 
